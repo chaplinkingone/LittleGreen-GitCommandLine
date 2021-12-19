295819bb60e6b51897c46000be0bcdc7d01e9f04
xiayi <xiayiaxy@icloud.com>
2021-12-19 16:03:50 +0800

    init

---
+++ LittleGreen-GitCommandLine/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LittleGreen-GitCommandLine
+//
+//  Created by 夏燚 on 2021/12/19.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
